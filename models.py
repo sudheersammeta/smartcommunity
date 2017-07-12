ffe1ead65d74885220c73d0f436ae0dcbec1e373
Sudheer <sammetasudheer89@gmail.com>
2017-07-11 23:10:23 -0700

    Update models.py
    added more tables to the application

--- models.py
+++ models.py
@@ -125,3 +125,167 @@ class Parent(UserMixin, db.Model):
 
     def __repr__(self):
         return '<Parent: {}>'.format(self.parentid)
+
+class Grade(db.Model):
+      """
+      Create grades table
+      """
+      __tablename__ = 'grades'
+
+      id = db.Column(db.Integer, primary_key=True)
+      grade = db.Column(db.Integer)
+      courseid = db.Column(db.Integer, db.ForeignKey('courses.id'))
+      schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+      def __repr__(self):
+         return '<Grade: {}>'.format(self.grade)
+
+class Query(db.Model):
+      """
+      Create queries table
+      """
+      __tablename__ = 'queries'
+
+      id = db.Column(db.Integer, primary_key=True)
+      senderid = db.Column(db.Integer, db.ForeignKey('members.id'))
+      receiverid = db.Column(db.Integer, db.ForeignKey('members.id'))
+      tag = db.Column(db.String(10))
+      description = db.Column(db.String(500))
+      reply = db.Column(db.String(500))
+      creationtime = db.Column(db.DateTime)
+      schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+      def __repr__(self):
+         return '<Query: {}>'.format(self.tag)
+
+class Message(db.Model):
+      """
+      Create messages table
+      """
+      __tablename__ = 'messages'
+
+      id = db.Column(db.Integer, primary_key=True)
+      senderid = db.Column(db.Integer, db.ForeignKey('members.id'))
+      receiverid = db.Column(db.Integer, db.ForeignKey('members.id'))
+      title = db.column(db.String(50))
+      description = db.Column(db.String(500))
+      reply = db.Column(db.String(500))
+      isread = db.Column(db.Boolean)
+
+      def __repr__(self):
+         return '<Message: {}>'.format(self.isread)
+
+class Broadcast(db.Model):
+      """
+      Create broadcasts table
+      """
+      __tablename__ = 'broadcasts'
+
+      id = db.Column(db.Integer, primary_key=True)
+      broadcastall = db.Column(db.Boolean)
+      description = db.Column(db.String(500))
+      creationtime = db.Column(db.DateTime)
+      schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+      def __repr__(self):
+         return '<Broadcast: {}>'.format(self.creationtime)
+
+class Forward(db.Model):
+      """
+      Create forwards table
+      """
+      __tablename__ = 'forwards'
+
+      id = db.Column(db.Integer, primary_key=True)
+      originalid= db.Column(db.Integer, db.ForeignKey('members.id'))
+      replacerid= db.Column(db.Integer, db.ForeignKey('members.id'))
+      expirytime = db.Column(db.DateTime)
+
+      def __repr__(self):
+         return '<Forward: {}>'.format(self.replacerid)
+
+class Mappingquery(db.Model):
+      """
+      Create mappingquery table
+      """
+      __tablename__ = 'mappings'
+
+      id = db.Column(db.Integer, primary_key=True)
+      tag= db.Column(db.String(10))
+      memberid= db.Column(db.Integer, db.ForeignKey('members.id'))
+
+      def __repr__(self):
+         return '<Mappingquery: {}>'.format(self.memberid)
+
+class Student(UserMixin, db.Model):
+        """
+        Create students table
+        """
+        __tablename__ = 'students'
+
+        id = db.Column(db.Integer, primary_key=True)
+        studentid = db.Column(db.Integer, db.ForeignKey('members.id'))
+        grade = db.Column(db.Integer, db.ForeignKey('grades.grade'))
+        section = db.Column(db.Integer)
+        schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+        def __repr__(self):
+            return '<Student: {}>'.format(self.studentid)
+
+class Course(db.Model):
+      """
+      Create courses table
+      """
+      __tablename__ = 'courses'
+
+      id = db.Column(db.Integer, primary_key=True)
+      coursename = db.Column(db.String(10))
+      schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+      def __repr__(self):
+          return '<Course: {}>'.format(self.coursename)
+
+class Teacher(UserMixin, db.Model):
+        """
+        Create teachers table
+        """
+        __tablename__ = 'teachers'
+
+        id = db.Column(db.Integer, primary_key=True)
+        teacherid = db.Column(db.Integer, db.ForeignKey('members.id'))
+        courseid = db.Column(db.Integer, db.ForeignKey('courses.id'))
+        onleave = db.Column(db.Boolean)
+        schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+        def __repr__(self):
+            return '<Teacher: {}>'.format(self.teacherid)
+
+class Staff(UserMixin, db.Model):
+        """
+        Create staffs table
+        """
+        __tablename__ = 'staffs'
+
+        id = db.Column(db.Integer, primary_key=True)
+        staffid = db.Column(db.Integer, db.ForeignKey('members.id'))
+        onleave = db.Column(db.Boolean)
+        schoolid = db.Column(db.Integer, db.ForeignKey('schools.id'))
+
+        def __repr__(self):
+            return '<Staff: {}>'.format(self.staffid)
+
+class Role(db.Model):
+    """
+    Create a Role table
+    """
+
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(60), unique=True)
+    description = db.Column(db.String(200))
+    employees = db.relationship('Employee', backref='role',
+                                lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role: {}>'.format(self.name)
